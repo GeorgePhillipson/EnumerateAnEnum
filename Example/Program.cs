ebd6e8c69dfd647498ae81fd8e0d4374b48ddbe8
George <gwphillipson@yahoo.co.uk>
2017-04-14 22:35:37 +0100

    Enumerate over an Enum

---
+++ Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Example
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            EnumerateOverDogs();
+        }
+
+        private enum Dogs
+        {
+            GermanShepherd,
+            GreatPyrenees,
+            IrishSetter,
+            Keeshond
+        }
+
+        private static void EnumerateOverDogs()
+        {
+            foreach (Dogs dog in Enum.GetValues(typeof(Dogs)))
+            {
+                Console.WriteLine(dog);
+            }
+            Console.Read();
+        }
+    }
+}
